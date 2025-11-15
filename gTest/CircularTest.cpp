f72f4a334fbc37ecace3efbc4c349e6624e37c26
MahmoudMohamedAli <mahmoudalielkout@gmail.com>
2025-11-15 14:05:14 +0100

    add cmakefile to build the project

---
+++ gTest/CircularTest.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+// #include "CircularBuffer.hpp"
+
+// int main()
+// {
+//     std::cout << "secondHello Circular Buffer" << std::endl;
+//     return 0;
+// }
+
+#include "CircularBuffer.hpp"
+#include <gtest/gtest.h>
+
+TEST(TEST_SUIT1, TESTCASE1)
+{
+
+    EXPECT_EQ(true, true);
+}
